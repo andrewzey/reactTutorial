14fb759572249fedb39b8602d1f3380f50a757c2
Andrew Zey <andrew@andrewzey.com>
2014-11-22 15:43:44 -0800

    complete step 1

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,15 @@
 'use strict';
 
 var React = require('react');
+
+var Box = React.createClass({
+  render: function(){
+    return (
+      <div>
+        Hello World!
+      </div>
+    );
+  }
+});
+
+React.render(<Box />, document.getElementById('content'));
