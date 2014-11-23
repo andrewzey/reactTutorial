7d8cb8b84e28583360999a9899807081c5ab25eb
Andrew Zey <andrew@andrewzey.com>
2014-11-22 19:35:39 -0800

    complete steps 10-12

--- src/app.jsx
+++ src/app.jsx
@@ -4,13 +4,13 @@ var React = require('react');
 
 var Box = React.createClass({
   handleClick: function(){
-    this.props.handleClick(this.props.index);
+    this.props.handleClick(this.props.colIndex);
   },
   render: function(){
     var style = {
       width: '100px',
       height: '100px',
-      backgroundColor: 'red'
+      backgroundColor: '#bababa'
     };
     return (
       <button style={style} onClick={this.handleClick}>
@@ -21,27 +21,13 @@ var Box = React.createClass({
 });
 
 var Row = React.createClass({
-  getInitialState: function(){
-    return {
-      clicks: 0,
-      values: ['-', '-', '-']
-    };
-  },
-  handleClick: function(index){
-    var newVal = (this.state.clicks % 2 === 1) ? 'O' : 'X'; 
-    var values = this.state.values;
-    if (values[index] === '-') {
-      values[index] = newVal;
-      this.setState({
-        values: values,
-        clicks: this.state.clicks + 1
-      });
-    }
+  handleClick: function(colIndex){
+    this.props.handleClick(this.props.rowIndex, colIndex);
   },
   render: function(){
-    var boxes = this.state.values.map(function(val, index){
+    var boxes = this.props.values.map(function(cell, index){
       return (
-        <Box value={val} key={index} index={index} handleClick={this.handleClick}/>
+        <Box value={cell} key={index} colIndex={index} handleClick={this.handleClick}/>
       );
     }.bind(this));
     return (
@@ -52,4 +38,40 @@ var Row = React.createClass({
   }
 });
 
-React.render(<Row />, document.getElementById('content'));
+var Board = React.createClass({
+  getInitialState: function(){
+    return {
+      clicks: 0,
+      values: [ 
+        ['-','-','-'],
+        ['-','-','-'],
+        ['-','-','-']
+      ]
+    }
+  },
+  handleClick: function(rowIndex, colIndex){
+    var newVal = (this.state.clicks % 2 === 1) ? 'O' : 'X'; 
+    var values = this.state.values;
+    if (values[rowIndex][colIndex] === '-') {
+      values[rowIndex][colIndex] = newVal;
+      this.setState({
+        values: values,
+        clicks: this.state.clicks + 1
+      });
+    }
+  },
+  render: function(){
+    var rows = this.state.values.map(function(row, index){
+      return (
+        <Row key={index} values={row} rowIndex={index} handleClick={this.handleClick}/>
+      );
+    }.bind(this));
+    return (
+      <div>
+        {rows}
+      </div>
+    );
+  }
+});
+
+React.render(<Board />, document.getElementById('content'));
