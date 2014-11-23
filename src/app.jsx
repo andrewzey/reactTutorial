e05da9f760b390c27051a9d5bb3b512477822feb
Andrew Zey <andrew@andrewzey.com>
2014-11-22 18:24:05 -0800

    complete step 5

--- src/app.jsx
+++ src/app.jsx
@@ -8,6 +8,18 @@ var Box = React.createClass({
       value: 'X'
     };
   },
+  componentDidMount: function(){
+    this.timer = setInterval(function(){
+      var oldVal = this.state.value;
+      var newVal = (oldVal === 'X') ? 'O' : 'X';
+      this.setState({
+        value: newVal
+      });
+    }.bind(this), 300);
+  },
+  componentWillUnmount: function(){
+    clearInterval(this.timer);
+  },
   render: function(){
     var style = {
       width: '100px',
